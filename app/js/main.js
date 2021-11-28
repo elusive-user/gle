ff10e6bded1e29d70122556624c9433b03a80029
elusive-user <batya.varfeysa.96@list.ru>
2021-11-28 13:35:01 +0300

    added more page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,43 @@
 $(function () {
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.01,
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
+  });
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    spacing: "7px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    
+  });
+
+  $(".star-big").rateYo({
+    starWidth: "17px",
+    spacing: "15px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+
   });
 
   var mixer = mixitup('.product__items');
